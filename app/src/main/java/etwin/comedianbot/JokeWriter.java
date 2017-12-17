638b92a2e8dfa1aecd9a113859781129f174d08b
probuse <nprobuse@gmail.com>
2017-12-17 18:37:55 +0300

    Added JokeWriter class

--- app/src/main/java/etwin/comedianbot/JokeWriter.java
+++ app/src/main/java/etwin/comedianbot/JokeWriter.java
@@ -19,6 +19,29 @@ public class JokeWriter {
                               "That way when i criticize him, i am a mile away and have his shoes."));
         jokeList.add(new Joke("What do you call a fish with no eye?",
                               "Fssssh"));
-        jokeList.add(new Joke("Have you heard the one about "));
+        jokeList.add(new Joke("Have you heard the one about the Corduroy pillow?",
+                              "It is making HEADLINES!"));
+        jokeList.add(new Joke("What is red and bad for your teeth?",
+                               "A brick."));
+
+        return jokeList;
+    }
+
+    public static ArrayList<Joke> getJokeListTwo(){
+        ArrayList<Joke> jokeList = new ArrayList<Joke>();
+
+        jokeList.add(new Joke("Why didn't the melons get married?",
+                              "Because they cantaloupe!"));
+        jokeList.add(new Joke("What did the cobbler say when the cat entered his shop?",
+                              "Shoe!"));
+        jokeList.add(new Joke("What did the Buddhist say to the hotdog vendor?",
+                              "Make me one with everything!"));
+        jokeList.add(new Joke("The face of a child can say it all",
+                              "especially the mouth part of the face."));
+        jokeList.add(new Joke("Why did the cookie go to the hospital",
+                              "Because he felt crummy."));
+        jokeList.add(new Joke("I intend to live forever.....",
+                              "So far, so good."));
+        return jokeList;
     }
 }
