187578aa9d2b471e97dd235228f142d8051dbd8b
probuse <nprobuse@gmail.com>
2017-12-17 19:10:33 +0300

    added ObjectActivity

---
+++ app/src/main/java/etwin/comedianbot/ObjectActivity.java
@@ -0,0 +1,19 @@
+package etwin.comedianbot;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class ObjectActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_object);
+
+        ArrayList<Joke> somejokes = JokeWriter.getJokeListOne();
+        JokeBot jokeBot = new JokeBot(somejokes);
+        jokeBot.tellJoke();
+    }
+}
